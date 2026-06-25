83172976cb1a295bb7de7f49b1f7ea9d3f58c398
id77 <9592236+id77@users.noreply.github.com>
2026-06-25 15:27:03 +0800

    Update jd_hd.js

--- Script/jd_hd.js
+++ Script/jd_hd.js
@@ -239,7 +239,7 @@ if ($request.url.includes('m=yunc_ticket&')) {
   html = html.replace(/opg:"1"/, 'opg:"0"');
   html = html.replace(/"open2":\d+/g, '"open2":0');
 }
-if ($request.url.includes('cc.zgg')) {
+if ($request.url.includes('https://cc.')) {
   html = html.replace(/NOT_PROGRESS/g, 'IN_PROGRESS');
   html = html.replace(/"canObtained":false/g, '"canObtained":true');
   html = html.replace(
@@ -249,6 +249,10 @@ if ($request.url.includes('cc.zgg')) {
   html = html.replace(
     /"obtainStartTime":".+?",/g,
     '"obtainStartTime":"2026-03-26 10:00:00",',
+  );
+  html = html.replace(
+    /"startTime":".+?",/g,
+    '"startTime":"2026-03-26 10:00:00",',
   );
 }
 
